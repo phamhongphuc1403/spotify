7a91f20d79a81fb52f69d2738771c076f3b11908
BotGodesports <phucdepzai594381672@gmail.com>
2021-10-07 18:19:29 +0700

    add now-playing html

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,37 @@ function openMenu() {
         
     }
 }
+
+
+
+
+
+
+
+const slider = document.getElementById("root__now-playing__player-control__playback-bar__range-slider")
+slider.oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #1db954 0%, #1db954 ' + value + '%,#535353 ' + value + '%, #535353 100%)'
+};
+
+slider.onmouseenter = function() {
+    const head = document.querySelector('head')
+    const link = document.createElement('link')
+    link.setAttribute('rel',"stylesheet")
+    link.setAttribute('href',"./assets/css/slider.css")
+    link.setAttribute('id',"slider")
+    head.appendChild(link)
+    
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #1db954 0%, #1db954 ' + value + '%,#535353 ' + value + '%, #535353 100%)'
+}
+
+slider.onmouseleave = function() {
+    const sliderHover = document.getElementById('slider')
+    sliderHover.remove()
+    
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #b3b3b3 0%, #b3b3b3 ' + value + '%,#535353 ' + value + '%, #535353 100%)'
+
+}
+
