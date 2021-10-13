249b839ff820feb74706d625bb9b98f3b8d2a297
BotGodesports <phamhongphuc1403@gmail.com>
2021-10-13 16:46:17 +0700

    responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 function openMenu() {
     document.getElementById('root__top__user').onclick = function() {
+        document.getElementById('root__top__user').style.backgroundColor = '#282828'
+
         if (document.querySelector('.root__top__user__drop-bar')) {
             document.querySelector('.root__top__user__drop-bar').remove()
         } else {
@@ -19,7 +21,6 @@ function openMenu() {
 
 function handleCurrentPlaylistHover() {
     const currentPlaylists = Array.from(document.getElementsByClassName('current-playlist'));
-    const pauseBtns = Array.from(document.getElementsByClassName('playing'))
     currentPlaylists.forEach(playlist => {
         playlist.onmouseover = function() {
             playlist.querySelector('.play-now-shadow').style.opacity= '1';
@@ -36,26 +37,67 @@ function handleCurrentPlaylistHover() {
 function handleTopContainerOpacity() {
     const rootTop = document.getElementById('root__top-container')
     const mainView = document.getElementById('root__main-view')
+    const root = document.getElementById('root')
+    
     mainView.onscroll = function() {
         if (100 - Math.ceil(mainView.scrollTop) <= 0) {
             rootTop.style.backgroundColor = `rgba(54, 50, 34, ${0.5 + - (100 - Math.ceil(mainView.scrollTop)) / 100})`;
         }
     }
+
+    root.onscroll = function() {
+        
+        if (100 - Math.ceil(root.scrollTop) <= 0) {
+            rootTop.style.backgroundColor = `rgba(54, 50, 34, ${0.5 + - (100 - Math.ceil(root.scrollTop)) / 100})`;
+        }
+        
+    }
 }
 
-function handleTopContainerWidth() {
-    const rootTop = document.getElementById('root__top-container')
-    const mainView = document.getElementById('root__main-view')
-    rootTop.style.width = mainView.offsetWidth + 'px';
+
+
+function handleResponsive() {
+    function handleTopContainerWidth() {
+        const rootTop = document.getElementById('root__top-container')
+        const mainView = document.getElementById('root__main-view')
+        rootTop.style.width = mainView.offsetWidth + 'px';        
+    }
+
+    function handleTextOverflow() {
+        const currentPlaylistTitle = document.querySelectorAll('#root__main-view__currently-playing__playlists > .current-playlist > .current-playlist__content > span')
+        const currentPlaylistWidth = document.querySelector('#root__main-view__currently-playing__playlists > .current-playlist').offsetWidth
+        
+        for (title of currentPlaylistTitle) {
+            title.style.width = `${currentPlaylistWidth - 164}px`
+        }
+    }
+
+    function handlePlaylists() {
+        const playlistImgs = document.getElementsByClassName('playlist-img')
+        for (let playlistImg of playlistImgs) {
+            playlistImg.style.width = document.querySelector('.playlist').offsetWidth - 32 + 'px'
+        }
+    }
     
-}
-
-function handleTopContainer() {
+    handleTextOverflow()
     handleTopContainerWidth()
-    handleTopContainerOpacity()
+    handlePlaylists()
+
+    window.onresize = function() {
+        handleTopContainerWidth() 
+        handleTextOverflow()
+        handlePlaylists()
+    }
 }
 
-handleTopContainer()
+
+handleTopContainerOpacity()
 openMenu()
 handleCurrentPlaylistHover()
+handleResponsive() 
 
+
+
+
+    
+    
